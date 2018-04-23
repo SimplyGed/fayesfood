39b96215fa73a3b6725d8aecbcdf5411612704f7
Ged Moretta <Ged.Moretta@Investec.co.uk>
2018-04-23 12:08:03 +0100

    add graph of authors

--- client/src/app/stats/stats.component.ts
+++ client/src/app/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 import { FoodService } from '../food/food.service';
 import { reduce, catchError, tap } from 'rxJs/operators';
@@ -6,60 +6,86 @@ import { Recipe } from '../food/food.model';
 import { map } from 'rxjs/operators';
 import { ByAuthor } from './stats.byauthor';
 import { of } from 'rxJs/observable/of';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'ff-stats',
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.scss']
 })
-export class StatsComponent implements OnInit, AfterViewInit {
-  series: any[];
+export class StatsComponent implements OnInit, OnDestroy {
+  options: any;
+  authors: Chart;
+  receipeSubscription: Subscription;
 
-  ngAfterViewInit(): void {
-    console.log(this.data);
+
+  data: ByAuthor[] = [{
+    name: 'Jamie Oliver',
+    y: 5
+  }, {
+    name: 'Gordon Ramsay',
+    y: 3
+  }, {
+    name: 'Anne Jones',
+    y: 11
+  }];
+
+
+  constructor(private foodService: FoodService) {
+    this.options = {
+      chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Recipes By Author'
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'Recipes',
+        data: {}
+      }]
+    };
   }
 
-  authors: Chart = new Chart({
-    chart: {
-      type: 'pie'
-    },
-    title: {
-      text: 'Recipes By Author'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [{
-      name: 'Recipes',
-      data: this.series
-      // [{
-      //   name: 'Jamie Oliver',
-      //   y: 5
-      // }, {
-      //   name: 'Gordon Ramsay',
-      //   y: 3
-      // }, {
-      //   name: 'Anne Jones',
-      //   y: 11
-      // }]
-    }]
-  });
-
-  data: ByAuthor[];
-
-  constructor(private foodService: FoodService) { }
-
   ngOnInit() {
-    this.foodService.getAllReceipes()
+
+    this.receipeSubscription = this.foodService.getAllReceipes()
       .subscribe(
-        data => {
-          this.data = data.reduce((arr: ByAuthor[], v: Recipe) =>
-            (arr[v.Author] = (arr[v.Author] || 0) + 1, arr), new Array<ByAuthor>());
-            
-          this.data.forEach(ba => {
-            this.series.push(ba);
-          });
-        });
+      data => {
+        const list = data.reduce((arr: ByAuthor[], v: Recipe) => {
+          const author = this.createOrFindAuthor(arr, v);
+          author.y += 1;
+
+          return arr;
+        }, new Array<ByAuthor>());
+
+        this.options.series[0].data = list;
+
+        this.authors = new Chart(this.options);
+      });
+  }
+
+  private createOrFindAuthor(array: ByAuthor[], recipe: Recipe): ByAuthor {
+    const name = (recipe.Author.length > 0 ? recipe.Author : 'UNKNOWN');
+
+    let found = array.find(i => i.name === name);
+
+    if (!found) {
+      const newAuthor = new ByAuthor();
+      newAuthor.name = name;
+      newAuthor.y = 0;
+      array.push(newAuthor);
+
+      found = newAuthor;
+    }
+
+    return found;
+  }
+
+  ngOnDestroy(): void {
+    this.receipeSubscription.unsubscribe();
   }
 
   // .pipe(
